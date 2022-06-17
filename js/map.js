d5f5b93b4293ca6b21a9acba73c244aa37a5d082
RomanLotocki <roman.lotocki@gmail.com>
2022-06-18 00:33:00 +0200

    dynamic markers on the map OK

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ const map = {
     init: function(){
         
         //Création de la map
-        var map = L.map('map').setView([18.045124, -63.019916], 5);
+        var map = L.map('map').setView([18.045124, -63.019916], 3);
 
         //Définition du layer
         var mainLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
@@ -29,16 +29,17 @@ const map = {
             'satellite': satelliteLayer
         }).addTo(map);
 
-        //testing array
+        //tableau de test
         let items = [
             {title : 'Todai-Ji', description : 'Le Todai-ji est un temple bouddhique situé à Nara, sur l\'île principale de Honshu. Ce grand temple de l\'est abrite notamment le bâtiment Daibutsu-den, connue pour être la plus grande construction en bois du monde et hébergeant un monumental Grand Bouddha assis en bronze.', image : './img/todai-ji-min.jpg', latitude : 34.689037, longitude : 135.840171},
-            {title : 'Saint Graal', description : 'Coupe ultime pour deguster un bon petit Chianti', image : './img/todai-ji-min.jpg', latitude : 38.665280, longitude : 27.399070}
+            {title : 'Saint Graal', description : 'Coupe ultime pour déguster un bon petit Chianti', image : './img/todai-ji-min.jpg', latitude : 38.665280, longitude : 27.399070}
         ];
 
+        //Création dynamique des marqueurs
         for(let item of items){
         var marker = L.marker([item.latitude, item.longitude]);
-        marker.addTo(map).bindPopup(item.title)
-        }
+        marker.addTo(map).bindPopup("<div class='popupMap'><h1>"+item.title+"</h1><p>"+item.description+"</p><img src='"+item.image+"' alt='"+item.title+"'></div>")
+        };
  
     }
 
